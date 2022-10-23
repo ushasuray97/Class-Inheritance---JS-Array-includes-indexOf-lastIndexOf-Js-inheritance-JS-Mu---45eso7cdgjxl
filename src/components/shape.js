0d2344109f18e472e426564767b712877dbbecd4
ushasuray97 <50068044+ushasuray97@users.noreply.github.com>
2022-10-23 11:56:01 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,12 @@
 // declare class
-
+class Shape{
+  var color='red';
+  calculateArea(){
+    console.log("I am in shape class");
+  }
+  drawShape(){
+    console.log("shape drawn");
+  }
+}
+module.exports Shape;
 // export class using module.exports
