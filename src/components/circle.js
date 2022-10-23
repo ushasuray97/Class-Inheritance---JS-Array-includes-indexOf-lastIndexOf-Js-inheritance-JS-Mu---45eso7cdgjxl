4f9f8472ede19228db1687fbb5880a86722740a7
ushasuray97 <50068044+ushasuray97@users.noreply.github.com>
2022-10-23 11:51:36 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,9 @@
 // import using require
-
+require('shape');
 // declare class
-
-
+class Circle extends Shape{
+  calculateArea(){
+    console.log("calculating area of circle");
+}
+module.exports Circle;
 // export class using module.exports
